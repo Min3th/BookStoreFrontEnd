ee6c604ccd3a29d07ffb5633f7437c853b50cda7
Mineth Weerasinghe <minethweerasinghe@mineth.local>
2025-01-12 14:08:56 +0530

    setting up asgardeo

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import bookcover from '../images/bookcover.jpg';
 
+
 const Card = () => {
   return (
-    <div className='w-[250px] h-[300px] bg-slate-400 shadow-md rounded-[10px] flex flex-col p-2'>
-      <div><img src={bookcover}/></div>
-      <div>Update</div>
-      <div>Delete</div>
+    <div className='w-[250px] h-[320px] bg-slate-400 shadow-md rounded-[10px] flex flex-col p-2 justify-center items-center'>
+      <div className='flex flex-row items-center justify-center'><img className='w-[200px] h-[230px]' src={bookcover}/></div>
+      <div className='flex flex-row w-[200px] h-[30px] justify-center items-center p-2 m-1 bg-green-300 cursor-pointer'>Update</div>
+      <div className='flex flex-row w-[200px] h-[30px] justify-center items-center p-2 m-1 bg-red-300 cursor-pointer'>Delete</div>
+      
     </div>
   )
 }
